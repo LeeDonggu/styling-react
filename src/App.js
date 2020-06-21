f1455b944c192831ae043e011bd93fb105b0c0be
LEEDONGGU <donggu19940717@gmail.com>
2020-06-21 18:38:20 +0900

    styled-components

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import React from "react";
 import logo from "./logo.svg";
 import "./App.css";
 import SassComponent from "./SassComponent";
+import CSSModule from "./CSSModule";
+import StyledComponent from "./StyledComponent";
 
 function App() {
   return (
     <div>
-      <SassComponent />
+      <StyledComponent />
     </div>
   );
 }
